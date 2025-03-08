b30083623cd4af10396883b064ff4de0da89b4ae
EkaterinaGorbunova <ekaterina.nalitova@gmail.com>
2025-03-07 19:36:51 -0800

    removed environment.ts and environment.prod.ts from .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  stripePublishableKey: 'pk_test_51R0BVHB19SrZnMXs7lAOCcqQ38UinX7xmDlHO6TIkelrSM6SFaTEEriGf62pHSrinBiAqHSktjMkcBBteledffRG005CQvSBRA',
+  apiUrl: '/api' // for local development through proxy
+};
